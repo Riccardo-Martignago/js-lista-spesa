04229b66d6416de0e7d668d306416a5ded00b32a
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-17 19:46:43 +0200

    Svolto esercizio lista spesa con ciclo while e stampa nel foglio html

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,11 @@
 console.log('Hello!')
+const shoppingList = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola','pesca','cipolla','pera']
+
+let i = 0
+const ulElement = document.querySelector("ul")
+while (i < shoppingList.length) {
+    const liApp = document.createElement("li")
+    const liElement = ulElement.appendChild(liApp)
+    liElement.innerHTML = shoppingList[i]
+    i++;
+}
